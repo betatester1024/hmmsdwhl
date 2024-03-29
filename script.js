0579e150e6ece9f5403000d4eac2530dbbbcd6dd
Glitch (glitch-hello-website) <none>
2024-03-29 19:02:53 +0000

    💓🎲 Checkpoint ./script.js:61849781/893 ./globalformat.css:61849781/332 ./.glitch-assets:61849781/345

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ function globalOnload() {
   let startTime = timeOfWriting - 4543000000n*yearMs; // 4.543 years before time of writing
   let finalTime = timeOfWriting + 7500000000n*yearMs; // 7.5 billion years after time of writing
   // Updates in real-time!
-  
+  let sunsetsLeft = (finalTime - BigInt(Date.now()))/BigInt(24*60*60*100);// timeLeft = finalTime - now)/ 
+  // has yours passed yet? 
+  let timeNow = new Date();
+  if (timeNow )
   let percentThru = (Date.now() - Number(startTime))/(Number(finalTime) - Number(startTime));
   console.log(percentThru)
   byId("progressInner").style.width = percentThru*100+"%";
-  
+  byId("progressText").innerText = ""
 }
